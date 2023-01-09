bd2e0df5ca8f70cc8f791083b16d53e38b506dd7
JoshuaHolzfurtner <68928033+JoshuaHolzfurtner@users.noreply.github.com>
2023-01-09 22:09:04 +0100

    anim Transitions implemented

--- Assets/PlayerSounds.cs
+++ Assets/PlayerSounds.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class PlayerSounds : MonoBehaviour
 {
     [SerializeField] AudioClip[] footstepSound = default;
+    private Animator animFace;
+    private int counterAnim;
+
+    public void Start()
+    {
+
+        animFace = GetComponent<Animator>();
+    }
+
     void footStep(int speechIndex)
     {
         //Debug.Log("halllapalooza");  //works handing speechindex further
         //Debug.Log(speechIndex);
         AudioManager.instance.source.PlayOneShot(footstepSound[speechIndex]);
+        animFace.SetTrigger("Trigger");
+        counterAnim = animFace.GetInteger("CurrentAnimation") + 1;
+        animFace.SetInteger("CurrentAnimation", counterAnim);
+
     }
 }
