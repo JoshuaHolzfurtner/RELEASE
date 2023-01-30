3f37b3f4dd228c352dcff6ac5edf9001946bae08
JoshuaHolzfurtner <68928033+JoshuaHolzfurtner@users.noreply.github.com>
2023-01-30 21:22:42 +0100

    Seat works with rotation

--- Assets/seatWaterrower.cs
+++ Assets/seatWaterrower.cs
@@ -6,6 +6,7 @@ public class seatWaterrower : MonoBehaviour
 {
     public Transform goalMatchUp;
     public float offsetX;
+    private Vector3 nullVec = new Vector3(0,90,0);
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,9 @@ public class seatWaterrower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3 (goalMatchUp.position.x + offsetX, transform.position.y, transform.position.z);
+        transform.localPosition = new Vector3 (goalMatchUp.localPosition.x + offsetX, transform.localPosition.y, transform.localPosition.z);
+        //transform.rotation = goalMatchUp.rotation;
+        //transform.rotation = Quaternion.Euler(nullVec);
+        //transform.localPosition = goalMatchUp.localPosition;
     }
 }
