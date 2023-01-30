a4df421663e758d39dd1fe3536bc3a26137306e5
JoshuaHolzfurtner <68928033+JoshuaHolzfurtner@users.noreply.github.com>
2023-01-31 00:37:21 +0100

    not yet tried out VR text video

--- Assets/Scripts/SwitchDIfferentVideos.cs
+++ Assets/Scripts/SwitchDIfferentVideos.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SwitchDIfferentVideos : MonoBehaviour
 {
@@ -23,6 +24,9 @@ public class SwitchDIfferentVideos : MonoBehaviour
     public int startFinish;
     public int startFinishDry;
     public int startRelease;
+
+    public TextMeshProUGUI chapterText;
+    public TextMeshProUGUI pageNumber;
 
 
     public GameObject[] charPics;
@@ -47,6 +51,7 @@ public class SwitchDIfferentVideos : MonoBehaviour
             CheckDryExercise(currentVideo);
             playSoundCurrentIndex(currentVideo);
             ChangePicMan();
+            pageNumber.text = currentVideo.ToString();
 
 
         }
@@ -54,12 +59,15 @@ public class SwitchDIfferentVideos : MonoBehaviour
         {
             currentVideo = 0;
             videos[10].SetActive(false);
-            videos[0].SetActive(true);
+            //videos[0].SetActive(true);
 
             videoMenu.SetActive(false);
             nextMenu.SetActive(true);
+            chapterText.text = "how to row";
+            pageNumber.text = currentVideo.ToString();
+
         }
-        
+
     }
 
     public void ReplayCurrentVideo()
@@ -82,6 +90,7 @@ public class SwitchDIfferentVideos : MonoBehaviour
         playSoundCurrentIndex(currentVideo);
         ChangePicMan();
         CheckDryExercise(currentVideo);
+        pageNumber.text = currentVideo.ToString();
 
     }
 
@@ -129,51 +138,71 @@ public class SwitchDIfferentVideos : MonoBehaviour
         {
             videos[0].SetActive(false);
             videos[1].SetActive(true);
+            chapterText.text = "How to row";
+
         }
         else if (currentVideo == startHandle)
         {
             videos[1].SetActive(false);
             videos[2].SetActive(true);
+            chapterText.text = "Grab handle";
+
         }
         else if (currentVideo == startGeneralTwo)
         {
             videos[2].SetActive(false);
             videos[3].SetActive(true);
+            chapterText.text = "How to row";
+
         }
         else if (currentVideo == startCatch)
         {
             videos[3].SetActive(false);
             videos[4].SetActive(true);
+            chapterText.text = "Catch";
+
         }
         else if (currentVideo == startCatchDry)
         {
             videos[4].SetActive(false);
             videos[5].SetActive(true);
+            chapterText.text = "Dry exercise Catch";
+
         }
         else if(currentVideo == startDrive)
         {
             videos[5].SetActive(false);
             videos[6].SetActive(true);
+            chapterText.text = "Drive";
+
         }
         else if (currentVideo == startDriveDry)
         {
             videos[6].SetActive(false);
             videos[7].SetActive(true);
+            chapterText.text = "Dry exercise Drive";
+
         }
         else if (currentVideo == startFinish)
         {
             videos[7].SetActive(false);
             videos[8].SetActive(true);
+            chapterText.text = "Finish";
+
         }
         else if (currentVideo == startFinishDry)
         {
             videos[8].SetActive(false);
             videos[9].SetActive(true);
+            chapterText.text = "Dry exercise Finish";
+
         }
         else if (currentVideo == startRelease)
         {
             videos[9].SetActive(false);
             videos[1].SetActive(true);
+            chapterText.text = "Release";
+
         }
         else
         {
@@ -187,51 +216,71 @@ public class SwitchDIfferentVideos : MonoBehaviour
         {
             videos[1].SetActive(false);
             videos[0].SetActive(true);
+            chapterText.text = "How to row";
+
         }
         else if (currentVideo == (startHandle-1))
         {
             videos[2].SetActive(false);
             videos[1].SetActive(true);
+            chapterText.text = "How to row";
+
         }
         else if (currentVideo == (startGeneralTwo-1))
         {
             videos[3].SetActive(false);
             videos[2].SetActive(true);
+            chapterText.text = "Grab handle";
+
         }
         else if (currentVideo == (startCatch-1))
         {
             videos[4].SetActive(false);
             videos[3].SetActive(true);
+            chapterText.text = "How to row";
+
         }
         else if (currentVideo == (startCatchDry-1))
         {
             videos[5].SetActive(false);
             videos[4].SetActive(true);
+            chapterText.text = "Catch";
+
         }
         else if (currentVideo == (startDrive-1))
         {
             videos[6].SetActive(false);
             videos[5].SetActive(true);
+            chapterText.text = "Dry exercise Catch";
+
         }
         else if (currentVideo == (startDriveDry-1))
         {
             videos[7].SetActive(false);
             videos[6].SetActive(true);
+            chapterText.text = "Drive";
+
         }
         else if (currentVideo == (startFinish-1))
         {
             videos[8].SetActive(false);
             videos[7].SetActive(true);
+            chapterText.text = "Dry exercise Drive";
+
         }
         else if (currentVideo == (startFinishDry-1))
         {
             videos[9].SetActive(false);
             videos[8].SetActive(true);
+            chapterText.text = "Finish";
+
         }
         else if (currentVideo == (startRelease-1))
         {
             videos[10].SetActive(false);
             videos[9].SetActive(true);
+            chapterText.text = "Dry exercise Finish";
+
         }
         else
         {
